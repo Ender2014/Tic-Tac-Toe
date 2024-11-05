706bd80597a05669effb0fad04d67294a950cbb9
Ender2014 <lau.zihao@gmail.com>
2024-11-05 12:53:32 +0800

    updated GameBoard methods

--- scripts.js
+++ scripts.js
@@ -1,16 +1,65 @@
 function GameBoard(){
+    // Properties;
     const rows = 3;
     const columns = 3;
-    
+    const board = [];
+    populateBoard();
+
+    const populateBoard = () => {
+        for (let i = 0; i < rows; i++){
+            board[i] = i;
+            for(let j = 0; j < columns; j++){
+                board[i].push(Cell(i, j));  
+            }
+        }
+    };
+
+    const getBoard = () => board;
+
+    const markCell = (row, column, player) => {
+
+    };
+
+    const printBoard = () => {
+        const updatedBoard = board.map((row) => {
+            row.map((cell) => {
+                cell.getCellMarker();
+            });
+        });
+
+        console.log(updatedBoard);
+    };
+
+    const clearBoard = () => {
+        board.splice(0, arr.length);
+        populateBoard()
+    };
+
+    return {
+        getBoard,
+        markCell,
+        printBoard,
+        clearBoard
+    }
 }
 
-function GameCell(position){
-    const cellPosition = position;
+function Cell(row, column){
     let cellMarker = 0;
+    const position = {row, column};
 
     const setCellMarker = (playerMarker) => {
         cellMarker = playerMarker;
     }
+
+    const getposition = () => position;
+
+    const getCellMarker = () => cellMarker;
+
+    return{
+        getposition,
+        getCellMarker,
+        setCellMarker
+    };
 }
 
 function createPlayer(name, marker){
@@ -25,10 +74,12 @@ function createPlayer(name, marker){
     const getScore = () => score;
 
     // Add score to player when they win;
-    const addScore = (points) => score++;
+    const addScore = (points) => score += points;
 
     // Player will make a move;
-    const makeMove = () => {};
+    const makeMove = () => {
+
+    };
         
     return {
         getName, 
@@ -39,6 +90,7 @@ function createPlayer(name, marker){
     };                     
 }
 
-function Game(){
+function GameController(){
 
 }
+
