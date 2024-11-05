6f80a1b76e835dfce42f338c86543719dfa53fba
Ender2014 <lau.zihao@gmail.com>
2024-11-05 12:17:53 +0800

    Created skeleton methods for creatPlayer fact ory

--- scripts.js
+++ scripts.js
@@ -1,16 +1,44 @@
-function Game(){
-
-}
-
-
 function GameBoard(){
+    const rows = 3;
+    const columns = 3;
     
 }
 
-function Board(){
-    
+function GameCell(position){
+    const cellPosition = position;
+    let cellMarker = 0;
+
+    const setCellMarker = (playerMarker) => {
+        cellMarker = playerMarker;
+    }
 }
 
-function Player(){
+function createPlayer(name, marker){
+    // Properties;
+    const playerName = name;
+    const playerMarker = marker;
+    let score = 0;
+
+    // Getter;
+    const getName = () => name;
+    const getMarker = () => marker;
+    const getScore = () => score;
+
+    // Add score to player when they win;
+    const addScore = (points) => score++;
+
+    // Player will make a move;
+    const makeMove = () => {};
+        
+    return {
+        getName, 
+        getMarker, 
+        makeMove,
+        getScore,
+        addScore
+    };                     
+}
+
+function Game(){
 
 }
